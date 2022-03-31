ab905c9515880086b7fd15218e08a7431f6c68a5
dasom <dasom228@gmail.com>
2022-04-01 00:50:24 +0900

    Add clipboard

--- frontend/src/components/Account.tsx
+++ frontend/src/components/Account.tsx
@@ -3,12 +3,16 @@ import { style } from '../data/style'
 import { MdOutlineContentCopy } from 'react-icons/md'
 import WalletDisconnect from './wallet/WalletDisconnect'
 import { AccountInfoType } from '../lib/type'
+import CopyToClipboard from 'react-copy-to-clipboard'
 
 interface AccountProps {
   info: AccountInfoType
 }
 
 const Account: FC<AccountProps> = ({ info: { account, balance } }) => {
+  const accountElipsis = (): string => {
+    return `${account.slice(0, 5)}...${account.slice(-4)}`
+  }
   return (
     <div className={`${style.roundBox}`}>
       <div className={`${style.innerContentInterval} pt-0`}>
@@ -19,10 +23,12 @@ const Account: FC<AccountProps> = ({ info: { account, balance } }) => {
       <div className={style.innerContentInterval}>
         <div
           className={`bg-indigo-900 text-white flex items-center w-min mx-auto ${style.roundContent}`}>
-          <p className="text-sm md:text-base">{account}</p>
-          <button type="button" className="px-2 -mr-2">
-            <MdOutlineContentCopy />
-          </button>
+          <p className="text-sm md:text-base">{accountElipsis()}</p>
+          <CopyToClipboard text={account}>
+            <button type="button" className="px-2 -mr-2">
+              <MdOutlineContentCopy />
+            </button>
+          </CopyToClipboard>
         </div>
       </div>
       <p className={`text-lg md:text-xl font-bold ${style.innerContentInterval}`}>{balance} ETH</p>
