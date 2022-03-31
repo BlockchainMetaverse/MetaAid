26ff197661ac6f9922bc86e5a9494ca476d5ad20
dasom <dasom228@gmail.com>
2022-03-31 15:07:46 +0900

    Add recoil

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import 'tailwindcss/tailwind.css'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
+// style
+import 'tailwindcss/tailwind.css'
+// languages
 import './lang/i18n'
+// wallet connet
 import { Web3ReactProvider } from '@web3-react/core'
 import { ExternalProvider, JsonRpcFetchFunc, Web3Provider } from '@ethersproject/providers'
+// state
+import { RecoilRoot } from 'recoil'
 
 // const MORALIS_APPLICATION_ID = process.env.REACT_APP_MORALIS_APPLICATION_ID || ''
 // const MORALIS_SERVER_URL = process.env.REACT_APP_MORALIS_SERVER_URL || ''
@@ -18,7 +23,9 @@ const getLibrary = (provider: ExternalProvider | JsonRpcFetchFunc): Web3Provider
 ReactDOM.render(
   <React.StrictMode>
     <Web3ReactProvider getLibrary={getLibrary}>
-      <App />
+      <RecoilRoot>
+        <App />
+      </RecoilRoot>
     </Web3ReactProvider>
   </React.StrictMode>,
   document.getElementById('root'),
