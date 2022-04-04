89095f3b71f4321eeb4f72cb8749125e0e130e2d
dasom <dasom228@gmail.com>
2022-04-04 20:53:52 +0900

    Refactoring

--- frontend/src/components/ItemEmpty.tsx
+++ frontend/src/components/ItemEmpty.tsx
@@ -12,7 +12,6 @@ const ItemEmpty: FC<ItemEmpty> = ({ mainTitle, subTitle, color }) => {
     <div className={`text-${color || 'gray-800'}`}>
       <div className="md:text-3xl py-2 md:py-3 text-center w-11/12 md:w-3/4 mx-auto">
         <img src="./images/empty.svg" alt="Empty NFT" />
-        {/* <img src="./images/hugo-no-connection.png" alt="Empty NFT" /> */}
       </div>
       <h2 className="text-xl tracking-tight font-black md:text-3xl py-2 md:py-3">{mainTitle}</h2>
       <p className="text-base md:text-xl font-normal py-2 md:py-3">{subTitle}</p>
