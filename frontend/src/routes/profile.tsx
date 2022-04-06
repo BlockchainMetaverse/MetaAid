cdfd2cf9d574aeebb663ba20b4e8489c15ff2f1f
dasom <dasom228@gmail.com>
2022-04-06 22:29:41 +0900

    Fix ShareCampain component location

--- frontend/src/routes/profile.tsx
+++ frontend/src/routes/profile.tsx
@@ -1,18 +1,14 @@
 import React, { FC, Suspense, useEffect } from 'react'
-import { useTranslation } from 'react-i18next'
 import { useNavigate } from 'react-router-dom'
 import { useRecoilValue } from 'recoil'
 import Account from '../components/Account'
 import CardNFTList from '../components/CardNFTList'
 import CardLoading from '../components/indicator/CardLoading'
-import Share from '../components/Share'
-import ShareCampain from '../components/ShareCampain'
 import { tokenIds } from '../data/response'
 import { style } from '../data/style'
 import { accountInfoState } from '../state/walletState'
 
 const Profile: FC = () => {
-  const { t } = useTranslation()
   const navigate = useNavigate()
 
   const accountInfo = useRecoilValue(accountInfoState)
@@ -41,10 +37,6 @@ const Profile: FC = () => {
             <CardNFTList type={'profile'} dataFormat={'video'} />
           </Suspense>
         </div>
-      </div>
-      <ShareCampain title={t('donation.sns_title')} hashtag={t('donation.sns_hashtag')} />
-      <div className={`${style.contentInterval} text-center`}>
-        <Share title={t('donation.sns_title')} hashtag={t('donation.sns_hashtag')} />
       </div>
     </div>
   )
